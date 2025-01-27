34257a9d67013a5b0c135e56d9613e7b023f97a1
0xzxcrypto <rizalsuzune@gmail.com>
2025-01-27 22:02:11 +0700

    Add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "TENEO",
+      script: "/usr/bin/expect",
+      args: "/home/hg680p/Teneoxyz/auto-input.exp",
+      cwd: "/home/hg680p/Teneoxyz"
+    }
+  ]
+};
